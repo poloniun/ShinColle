2a7e30a3ca22420cd13980a9b5a730a053c22eb4
misaka10843 <69132853+misaka10843@users.noreply.github.com>
2022-04-07 11:12:36 +0800

    Create RenderTileEntityItem.java

--- src/main/java/com/lulan/shincolle/client/render/item/RenderTileEntityItem.java
+++ src/main/java/com/lulan/shincolle/client/render/item/RenderTileEntityItem.java
@@ -1,13 +1,13 @@
 package com.lulan.shincolle.client.render.item;
 
 import com.lulan.shincolle.init.ModBlocks;
-import com.lulan.shincolle.tileentity.TileEntityDesk;
-import com.lulan.shincolle.tileentity.TileEntitySmallShipyard;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import com.lulan.shincolle.tileentity.TileEntityDesk;
+import com.lulan.shincolle.tileentity.TileEntitySmallShipyard;
+import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 
 /** tile entity itemblock renderer
  *
@@ -18,31 +18,50 @@ import net.minecraft.tileentity.TileEntity;
  */
 public class RenderTileEntityItem extends TileEntityItemStackRenderer
 {
+	private static final TileEntity SmallShipyard = new TileEntitySmallShipyard();
+	private static final TileEntity Desk = new TileEntityDesk();
+	private final TileEntityItemStackRenderer parent;
+
+	public RenderTileEntityItem(TileEntityItemStackRenderer parent) {
+		this.parent = parent;
+	}
 	
-	private static TileEntity SmallShipyard = new TileEntitySmallShipyard();
-	private static TileEntity Desk = new TileEntityDesk();
-	
-	
-    @Override
-    public void renderByItem(ItemStack itemStack)
-    {
-        Block block = Block.getBlockFromItem(itemStack.getItem());
-        
-        //mod特製模型的tile entity
-        if (block == ModBlocks.BlockSmallShipyard)
-        {
+	@Override
+	public void renderByItem(ItemStack itemStack)
+	{
+		//load tile entity as item model
+		if (itemStack.getItem() == Item.getItemFromBlock(ModBlocks.BlockSmallShipyard))
+		{
 			TileEntityRendererDispatcher.instance.render(SmallShipyard, 0D, 0D, 0D, 0F);
-        }
-        else if (block == ModBlocks.BlockDesk)
-        {
+		}
+		else if (itemStack.getItem() == Item.getItemFromBlock(ModBlocks.BlockDesk))
+		{
 			TileEntityRendererDispatcher.instance.render(Desk, 0D, 0D, 0D, 0F);
-        }
-        //其他mc的tile entity
-        else
-        {
-            super.renderByItem(itemStack);
-        }
-    }
-    
-    
+		}
+		//if neither of the above, render item normally
+		else
+		{
+			parent.renderByItem(itemStack);
+		}
+	}
+	
+	@Override
+	public void renderByItem(ItemStack itemStack, float partialTicks)
+	{
+		//load tile entity as item model
+		if (itemStack.getItem() == Item.getItemFromBlock(ModBlocks.BlockSmallShipyard))
+		{
+			TileEntityRendererDispatcher.instance.render(SmallShipyard, 0D, 0D, 0D, 0F);
+		}
+		else if (itemStack.getItem() == Item.getItemFromBlock(ModBlocks.BlockDesk))
+		{
+			TileEntityRendererDispatcher.instance.render(Desk, 0D, 0D, 0D, 0F);
+		}
+		//if neither of the above, render item normally
+		else
+		{
+			parent.renderByItem(itemStack, partialTicks);
+		}
+	}
+	
 }
